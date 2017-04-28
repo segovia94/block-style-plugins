60f4860e8f85313999b4e425dd9150e61e923c04
Mark Miller <segovia94@780142.no-reply.drupal.org>
2017-04-28 14:59:15 -0700

    test for the sample

---
+++ tests/src/Unit/Plugin/BlockStyle/SampleBlockStyleTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\Tests\block_style_plugins\Unit\Plugin\BlockStyle;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\block_style_plugins\Plugin\BlockStyle\SampleBlockStyle;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\EntityRepository;
+
+/**
+ * @coversDefaultClass \Drupal\block_style_plugins\Plugin\BlockStyle
+ * @group block_style_plugins
+ */
+class SampleBlockStyleTest extends UnitTestCase
+{
+
+  /**
+   * @var \Drupal\block_style_plugins\Plugin\BlockStyle\SampleBlockStyle
+   */
+  protected $plugin;
+
+  /**
+   * Create the setup for constants and configFactory stub
+   */
+  protected function setUp()
+  {
+    parent::setUp();
+
+    // stub the Iconset Finder Service
+    $entityRepository = $this->prophesize(EntityRepository::CLASS);
+
+    $configuration = [];
+    $plugin_id = 'block_style_plugins';
+    $plugin_definition['provider'] = 'block_style_plugins';
+
+    $this->plugin = new SampleBlockStyle(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $entityRepository->reveal()
+    );
+
+    // Create a translation stub for the t() method
+    $translator = $this->getStringTranslationStub();
+    $this->plugin->setStringTranslation($translator);
+  }
+
+  /**
+   * Tests the defaultStyles method.
+   *
+   * @see ::defaultStyles()
+   */
+  public function testDefaultStyles() {
+    $expected = ['sample_class' => ''];
+    $default = $this->plugin->defaultStyles();
+
+    $this->assertArrayEquals($expected, $default);
+  }
+
+  /**
+   * Tests the formElements method.
+   *
+   * @see ::formElements()
+   */
+  public function testFormElements() {
+    $form = [];
+    $formState = $this->prophesize(FormStateInterface::CLASS);
+
+    $default = $this->plugin->formElements($form, $formState->reveal());
+
+    $this->assertArrayHasKey('sample_class', $default);
+    $this->assertArrayHasKey('#default_value', $default['sample_class']);
+  }
+
+}
